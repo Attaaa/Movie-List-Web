a54853d181c0cbb1804adf4c6285962ccbb52f07
Attaaa <hatta.eka.p@gmail.com>
2019-02-02 14:39:58 +0700

    ui fix

--- index.php
+++ index.php
@@ -4,12 +4,50 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<title>MovieW</title>
+    <title>MovieW</title>
 
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link href="css/all.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
     
 </head>
 <body>
-    <div class="test"></div>
+    
+    <div class="nav shadow">
+        <ul class="nav container-fluid">
+            <li class="nav-item hamburger">
+                <a href="#" class="nav-link text-light d-md-none d-block"><i class="fas fa-bars"></i></a>
+            </li>
+            <li class="nav-item title">
+                <a href="#" class="nav-link text-light d-md-none d-block disable">MOVIEW</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link text-light d-md-block d-none">HOME</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link text-light d-md-block d-none">FEATURED</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link text-light d-md-block d-none">CATEGORY</a>
+            </li>
+            <li class="nav-item ml-auto">
+                <a href="#" class="nav-link text-light"><i class="fas fa-search"></i></a>
+            </li>
+        </ul>
+    </div>
+
+    <div class="main container-fluid p-5">
+        <div class="row px-sm-5 px-3" id="movie-list">
+            
+        </div>
+    </div>
+
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+    <script src="js/main.js"></script>
+
 </body>
 </html>
 
---
+++ js/main.js
@@ -0,0 +1,31 @@
+$.ajax({
+    url: 'http://www.omdbapi.com',
+    type: 'GET',
+    dataType: 'json',
+    data: {
+        'apikey': '73d280cb',
+        's': 'avengers'
+    },
+    success: function(result){
+        if (result.Response === "True") {
+            let movie_list = result.Search;
+            console.log(movie_list);
+
+            $.each(movie_list, function(i,data){
+                $('#movie-list').append(`
+                    <div class="col-xl-2 col-lg-3 col-md-4 col-sm-5 col-12 mb-4 p-2">
+                        <div class="card">
+                            <img src="`+ data.Poster +`" class="card-img-top" alt="Test">
+                            <div class="card-body text-center">
+                                <h5 class="card-title">`+ data.Title +`</h5>
+                                <a href="#" class="btn btn-gradient px-5">Detail</a>
+                            </div>
+                        </div>
+                    </div>
+                `);
+            })
+        }
+        
+    }
+});
+
